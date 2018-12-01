ecbe44c78199ed5ddd8b7239cc495b907d28c52c
fernandezeric <ericfernandezgonzalez@gmail.com>
2018-12-01 15:48:14 -0300

    parte1.x

--- index.php
+++ index.php
@@ -5,11 +5,41 @@
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1">
    <link rel="stylesheet" href="/style/styles.css">
-   <title>CSS MenuMaker</title>
+   <title>PACIENTE.</title>
 </head>
 <body>
 
-<?php include 'menu.html';?>
+<?php include 'menu.html';
+	$conexion = pg_connect("host=bdd.inf.udec.cl port=5432 dbname=bdi2018a user=bdi2018a password=bdi2018a")
+    or die ("Fallo!!!!");
+    $search_path = "SET search_path TO proyecto"; //En caso de que tengan una base de datos distinta del esquema public
+		//$query="show search_path;";
+		pg_query($conexion,$search_path);
+		$query="SELECT * FROM persona WHERE run='17872056'";
+		$rs= pg_query($conexion, $query);
+			if ($rs) {
+				echo "<table class=\"table\" align=''rules=all >" ;
+				echo "<tr>";
+				echo "<th scope=\"col\">Datos paciente</th>";
+				//echo "<th scope=\"col\">Nombre</th>";
+				//echo "<th scope=\"col\">Fecha Nacimiento</th>";
+				//echo "<th scope=\"col\">Direccion</th>";
+				//echo "<th scope=\"col\">Correo Electronico</th>";
+				//echo "<th scope=\"col\">Telefono</th>";
+				echo "</tr>";
+				while ($obj = pg_fetch_object($rs)) {
+				echo "<tr><td>"."Run: "."</td><td>".$obj->run."</td>";
+				echo "<tr><td>"."Nombre: "."</td><td>".$obj->nombre."</td>";
+				echo "<tr><td>"."Fecha Nacimiento: "."</td><td>".$obj->fecha_nacimiento."</td>";
+				echo "<tr><td>"."Direccion: "."</td><td>".$obj->direccion."</td>";
+				echo "<tr><td>"."Correo Electronico:  "."</td><td>".$obj->correo_electronico."</td>";
+				echo "<tr><td>"."Telefono: "."</td><td>".$obj->telefono."</td>";
+				echo "</tr>";
+				}
+				echo "</table>";
+			}
+		
+?>
 
 </body>
 <html>
