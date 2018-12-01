6af980c1480751803897b847fbe137f2584b24dd
Rodrigo Cordova Whiteley <r.a.c.whiteley@gmail.com>
2018-12-01 13:00:16 -0300

    template

--- index.php
+++ index.php
@@ -9,14 +9,7 @@
 </head>
 <body>
 
-<div id='cssmenu'>
-<ul>
-   <li class='active'><a href='#'>Home</a></li>
-   <li><a href='#'>Products</a></li>
-   <li><a href='#'>Company</a></li>
-   <li><a href='#'>Contact</a></li>
-</ul>
-</div>
+<?php include 'menu.html';?>
 
 </body>
 <html>
