ff130eb0ef2631ae024b897f6bc960c5256cf94f
Rodrigo Cordova Whiteley <r.a.c.whiteley@gmail.com>
2018-12-02 01:57:05 -0300

    menu.php cambiado

--- menu.php
+++ menu.php
@@ -3,7 +3,8 @@
     <ul>
         <li class='active'><a href='/index.php'>Paciente</a></li>
         <li><a href='/atenciones.php'>Atenciones</a></li>
-        <li><a href='/receta.php?$_COOKIE["rut"]'>Recetas</a></li>
+        <li><a href='/receta.php'>Recetas</a></li>
+        <li><a href='/licencias.php'>Licencias</a></li>
         <li><a href='#'>Archivos</a></li>
         <li><a href='#'>Parientes</a></li>
         <li><a href='/logout.php'>Cerrar sesión</a></li>
